37534aea3a0226d99e91c79fe3869050d728a2e9
Lulo002 <72836746+Lulo002@users.noreply.github.com>
2022-05-04 18:44:35 -0300

    Update Programa.py
    Agregué verificaciones de datos (menos en los pesos de los camiones, no sabia como)
    Falta el 8 que no anda

--- Programa.py
+++ Programa.py
@@ -51,10 +51,15 @@ def menu_administraciones():
     print("\t F- SUCURSALES")
     print("\t G- PRODUCTO POR TITULAR ")
     print("\t V- VOLVER AL MENU PRINCIPAL")
-    select = input("OPCION DESEADA: \n").upper()
-    if select == ("A" or "B" or "C" or "D" or "E" or "F" or "G"):
+
+    seleccion = input("OPCION DESEADA: \n")
+    while seleccion != "A" and seleccion != "B" and seleccion != "C" and seleccion != "V" and seleccion != "V":
+        print("Respuesta no aceptada.\nTip: verifique mayusculas")
+        seleccion = input("OPCION DESEADA: \n")
+
+    if seleccion == "A" or seleccion == "B" or seleccion == "C" or seleccion == "D" or seleccion == "E" or seleccion == "F" or seleccion == "G":
         menu_de_opciones()
-    if select == "V":
+    elif seleccion == "V":
         menu_principal()
 
 
@@ -66,12 +71,16 @@ def menu_de_opciones():
     print("\t C- CONSULTA")
     print("\t M- MODIFICACION")
     print("\t V- VOLVER AL MENU ANTERIOR")
-    seleccionarda = input("OPCION DESEADA: \n").upper()
-    if seleccionarda == ("A" or "B" or "C" or "M"):
-        print("\t Esta funcionalidad esta en construccion\n")
+    seleccion = input("OPCION DESEADA: \n")
+    while seleccion != "A" and seleccion != "B" and seleccion != "C" and seleccion != "M" and seleccion != "V":
+        print("Respuesta no aceptada.\nTip: verifique mayusculas")
+        seleccion = input("OPCION DESEADA: \n")
+
+    if seleccion == "A" or seleccion == "B" or seleccion == "C" or seleccion == "M":
+        print("Esta funcionalidad esta en construccion\n")
         os.system("pause")
         menu_de_opciones()
-    if seleccionarda == "V":
+    elif seleccion == "V":
         menu_administraciones()
 
 
@@ -85,57 +94,91 @@ def menu_recepcion():
     global PATENTE_MAS_MAIZ
 
     os.system("cls")
-    otro_Camion = "S"
+    otro_Camion = "-"
+    mayor_soja = 0
+    mayor_maiz = 0
+    tipo_Cargamento = '-'
+    peso_bruto_temp = 0
+    tara_temp = 0
 
     while otro_Camion != "N":
-        print("SE HA RECIBIDO UN CAMION")
-        patente = int(input("\t NUMERO DE PATENTE: "))
+        print("\nSE HA RECIBIDO UN CAMION")
+        patente = input("\t NUMERO DE PATENTE: ")
         CAMIONES = CAMIONES + 1
 
-        print("OPCIONES DE CARGAMENTO:")
-        print("\t 1 - SOJA")
-        print("\t 2 - MAIZ")
-        tipo_Cargamento = int(input())
+        while tipo_Cargamento != '1' and tipo_Cargamento != '2':
+            print("\nOPCIONES DE CARGAMENTO:")
+            print("\t 1 - SOJA")
+            print("\t 2 - MAIZ")
+            tipo_Cargamento = input("\nSeleccion: ")
+            if tipo_Cargamento != '1' and tipo_Cargamento != '2':
+                print("\nRespuesta no aceptada. Vuelva a intentarlo.\n")
 
-        peso_bruto_temp = int(input("INGRESO PESO BRUTO: "))
-        tara_temp = int(input("INGRESE SU TARA: "))
+        while peso_bruto_temp <= 0:
+            peso_bruto_temp = float(input("\nINGRESE PESO BRUTO: "))
+            if peso_bruto_temp <= 0:
+                print("\nDebe ingresar un valor positivo.\n")
+
+        while tara_temp <= 0 or tara_temp > peso_bruto_temp:
+            tara_temp = float(input("\nINGRESE SU TARA: "))
+            if tara_temp <= 0:
+                print("\nDebe ingresar un valor positivo.\n")
+            if tara_temp > peso_bruto_temp:
+                print("\nDebe ingresar una tara mayor al peso bruto.\n")
+
         neto_temp = peso_bruto_temp - tara_temp
+
         if tipo_Cargamento == 1:
-            CAMIONES_SOJA += 1
-            PESO_SOJA += neto_temp
-            if PATENTE_MAS_SOJA < neto_temp:
-                PATENTE_MAS_SOJA = neto_temp
+            global CAMIONES_SOJA
+            CAMIONES_SOJA = CAMIONES_SOJA + 1
+            PESO_SOJA = PESO_SOJA + neto_temp
+            if mayor_soja < neto_temp:
+                PATENTE_MAS_SOJA = patente
+                mayor_soja = neto_temp
 
         if tipo_Cargamento == 2:
-            CAMIONES_MAIZ += 1
-            PESO_MAIZ += neto_temp
-            if PATENTE_MAS_MAIZ < neto_temp:
-                PATENTE_MAS_MAIZ = neto_temp
+            CAMIONES_MAIZ = CAMIONES_MAIZ + 1
+            PESO_MAIZ = PESO_MAIZ + neto_temp
+            if mayor_maiz < neto_temp:
+                PATENTE_MAS_MAIZ = patente
+                mayor_maiz = neto_temp
 
-        print("EL PESO NETO DEL CAMION " + str(CAMIONES) + " ES: ", neto_temp)
-
-        otro_Camion = input("¿DESEA AGREGAR OTRO CAMION? (S/N)")
+        print("\nEL PESO NETO DEL CAMION " + str(CAMIONES) + " ES: ", neto_temp)
+        while otro_Camion != 'S' and otro_Camion != 'N':
+            otro_Camion = input("\n¿DESEA AGREGAR OTRO CAMION? (S/N)\n")
+            if otro_Camion != 'S' and otro_Camion != 'N':
+                print("\nRespuesta no aceptada.\nTip: verifique mayusculas\n")
+        if otro_Camion == 'S':
+            tipo_Cargamento = '-'
+            peso_bruto_temp = 0
+            tara_temp = 0
+            otro_Camion = '-'
 
 
 # programa main
 
-seleccion = -1
-while seleccion != 0:
+seleccion = '-'
+while seleccion != '0':
 
     menu_principal()
-    seleccion = int(input("SELECCIONAR OPCION: \n"))
+    seleccion = input("SELECCIONAR OPCION: \n")
 
-    if seleccion == 1:
+    while seleccion != '0' and seleccion != '1' and seleccion != '2' and seleccion != '3' and seleccion != '4' and seleccion != '5' and seleccion != '6' and seleccion != '7' and seleccion != '8':
+        print("\nRespuesta no aceptada. Se selecciona ingresando un numero del 0 al 8\n")
+        seleccion = input("SELECCIONAR OPCION: \n")
+
+    if seleccion == '1':
         menu_administraciones()
 
-    if seleccion == (2 or 4 or 5 or 6 or 7):
-        print("\t Esta funcionalidad esta en construccion\n")
+    elif seleccion == ('2' or '4' or '5' or '6' or '7'):
+        print("\nEsta funcionalidad esta en construccion\n")
         os.system("pause")
 
-    if seleccion == 3:
+    elif seleccion == '3':
         menu_recepcion()
 
-    if seleccion == 8:
+    # De este solo anda la cant total de camiones ~Lucio
+    elif seleccion == '8':
         os.system("cls")
         print("\nLA CANTIDAD TOTAL DE CAMIONES ES: ")
         print(CAMIONES)
@@ -157,5 +200,7 @@ while seleccion != 0:
         print(PATENTE_MAS_MAIZ)
         os.system("pause")
 
-    if seleccion == 0:
+    elif seleccion == '0':
         print("FIN DEL PROGRAMA")
+        os.system("pause")
+    
